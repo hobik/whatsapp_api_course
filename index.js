9b243c39dc33de0327bd89459b24e36735b3bcbf
duygu <duygu@duygu-MacBook-Air.local>
2023-02-17 17:24:05 +0300

    LocalAuth Strategy Completed

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const {Client} = require('whatsapp-web.js')
+const {Client, LocalAuth} = require('whatsapp-web.js')
 const qrcode = require('qrcode-terminal')
 
-const client = new Client()
+const client = new Client({
+    authStrategy: new LocalAuth()
+})
 
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true})
+    qrcode.generate(qr, {small: true}) 
 
 })
 
 client.on('ready',()=>{
     console.log("Client is Ready !!")
 })
-
+ 
 client.on('message', message =>{
     console.log("I've this message:", message.body)
 })
